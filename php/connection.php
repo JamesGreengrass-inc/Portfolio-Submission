290adbc481286430721643b5b0fe419014bb2815
James Greengrass <101417323+JamesGreengrass@users.noreply.github.com>
2022-09-05 17:58:55 +0100

    Added Sanitization
    I have filtered the input and spent a lot of time troubleshooting why I can't get access to the database.

--- php/connection.php
+++ php/connection.php
@@ -1,12 +1,13 @@
 <?php
 
 try {
-    $db = new PDO ("mysql:host=localhost;dbname=jamesgre_portfolio", "jamesgre_jamesgreengrass","WorkHabits2!");
-    //$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db = new PDO ("mysql:host=localhost;dbname=jamesgre_portfolio;port=3306", "jamesgre_jamesgreengrass", "WorkHabits2!");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected to the database!";
 } catch(Exception $e) {
     echo "Unable to connect. Error: ";
     echo $e->getMessage();
     exit;
 }
+
 ?>
--- php/server-side.php
+++ php/server-side.php
@@ -2,12 +2,6 @@
 
 include('inc/functions.php');
 
-// if(empty($_POST)) {
-
-// } else {
-//   $_POST = array();  
-// }
-
 $fnameErr = $lnameErr = $emailErr = $subjectErr = $messageErr = "";
 $fname = $lname = $email = $subject = $message = "";
 
@@ -17,24 +11,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fnameErr = "First name is required";
   } else {
     $fname = test_input($_POST["fname"]);
+    filter_input(INPUT_POST, "fname" , FILTER_SANITIZE_STRING);
   }
 
   if (empty($_POST["lname"])) {
     $lnameErr = "Last name is required";
   } else {
     $lname = test_input($_POST["fname"]);
+    $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
   }
 
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
   } else {
     $email = test_input($_POST["email"]);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
   }
 
   if (empty($_POST["subject"])) {
     $subjectErr = "Subject is required";
   } else {
     $subject = test_input($_POST["subject"]);
+    $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
   }
 
   if (empty($_POST["message"])) {
@@ -43,6 +41,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = test_input($_POST["message"]);
   }
 
-  var_dump($_POST);
 }
+
 ?>
